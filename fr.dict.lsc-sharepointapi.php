ea550497ead47251727e05fdf9cc4b93375439e7
Molkobain <lajarige.guillaume@free.fr>
2019-07-11 14:04:55 +0200

    Add french translations

---
+++ fr.dict.lsc-sharepointapi.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2019 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('FR FR', 'French', 'Français', array(
+	// Dictionary entries go here
+	'Class:SharepointAPI/Attribute:SharepointTab' => 'Fichiers Sharepoint',
+
+	'dictDocFolder' => 'Lien vers le répertoire de documents',
+	'dictFolderFile' => 'Répertoires et fichiers',
+	'dictPleaseAcc' => 'Merci de n\'acceder à ce fichier que depuis iTop',
+	'dictNoFiles' => 'Aucun fichier ou répertoire dans ce dossier, ou la Librairie de Documents n\'existe pas. Veuillez vérifier la Librairie de Documents suivante dans Sharepoint Online :',
+));
